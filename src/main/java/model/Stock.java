c2b5b9bcc1f66352efdf9e7562718a123edf4fe2
=Andrei <=balasca.andrei@gmail.com>
2019-02-09 15:23:53 +0200

    added stock class

---
+++ src/main/java/model/Stock.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Stock {
+
+    private int idStock;
+    private Product produs;
+    private int quantity;
+
+    public Stock(int idStock, Product produs, int quantity) {
+        this.idStock = idStock;
+        this.produs = produs;
+        this.quantity = quantity;
+    }
+
+    public int getIdStock() {
+        return idStock;
+    }
+
+    public void setIdStock(int idStock) {
+        this.idStock = idStock;
+    }
+
+    public Product getProdus() {
+        return produs;
+    }
+
+    public void setProdus(Product produs) {
+        this.produs = produs;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
